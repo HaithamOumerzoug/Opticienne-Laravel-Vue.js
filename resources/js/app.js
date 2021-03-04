76a4ebc7ec3d8c28d82d5e18846ed3a1ee4b83f9
OUMERZOUG Haitham <haithamoumerzoug31@gmail.com>
2021-03-04 17:10:30 +0100

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,3 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 const { default: Axios } = require('axios');
 
 require('./bootstrap'); 
@@ -12,25 +6,11 @@ import Vue from 'vue';
 
 window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('add-commande', require('./components/AddCommandeComponent.vue').default);
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
+const url="http://127.0.0.1:8000"
 const app = new Vue({
     el: '#app',
     data:{
@@ -105,7 +85,7 @@ const app = new Vue({
             var x=0;
             for(var i=0;i<this.commandes.length;i++){
                 x++;
-                Axios.post('http://127.0.0.1:8000/commandes',this.commandes[i])
+                Axios.post(`${url}/commandes`,this.commandes[i])
                 .then(response => {
                     if(response.data.etat && x==this.commandes.length){
                         document.location.href="http://127.0.0.1:8000/commandes"
@@ -122,7 +102,7 @@ const app = new Vue({
             var c=0;
             for(var i=0;i<this.stocks.length;i++){
                 c++;
-                Axios.post('http://127.0.0.1:8000/stocks',this.stocks[i])
+                Axios.post(`${url}/stocks`,this.stocks[i])
                 .then(response => {
                     if(response.data.etat && c==this.stocks.length){
                         document.location.href="http://127.0.0.1:8000/stocks"
@@ -136,7 +116,7 @@ const app = new Vue({
             
                 this.commandes.forEach(commande => {
                     if(commande.article !="" && commande.Qte_cmd>0){
-                        Axios.get('http://127.0.0.1:8000/getprice/'+commande.article+'/'+commande.Qte_cmd)
+                        Axios.get(`${url}/getprice/`+commande.article+'/'+commande.Qte_cmd)
                         .then(response => {
                             commande.price=response.data;
                             
@@ -149,7 +129,7 @@ const app = new Vue({
         },
         getcommandes(){
             if(this.allCommandes.length<1 ){
-                Axios.get('http://127.0.0.1:8000/getcommandes')
+                Axios.get(`${url}/getcommandes`)
                 .then(response => {
                     for(var i=0;i<this.c;i++)this.affichenewcommande();
 
@@ -169,7 +149,7 @@ const app = new Vue({
             
         },
         getnumbercommande(){
-            Axios.get('http://127.0.0.1:8000/getcommandes')
+            Axios.get(`${url}/getcommandes`)
                 .then(response => {
                     this.c=response.data.length;
                 }).catch(error => {console.error('errors: ',error);})
